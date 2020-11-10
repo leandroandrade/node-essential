52b81a3a0945df391593a8bf6123788fa2376e4d
leandroandrade <leandro.andrade2588@gmail.com>
2020-11-10 10:34:43 -0300

    add some tests

--- test/controllers/error-controller.test.js
+++ test/controllers/error-controller.test.js
@@ -4,9 +4,17 @@ const app = require('../../src/app');
 
 describe('simple error test', () => {
     it('should return error', async done => {
-        const { status } = await request(app).get('/api/v1/error');
+        const { status, body } = await request(app).get('/api/v1/error');
 
         expect(status).toBe(400);
+
+        const { mensagens } = body;
+        expect(mensagens.length).toBe(1);
+
+        const [mensagem] = mensagens;
+        const { codigo, mensagem: detalhe } = mensagem;
+        expect(codigo).toBe(400);
+        expect(detalhe).toBe('This is a simple error example!');
 
         done();
     });
